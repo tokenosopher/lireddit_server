83e8e44056b6aba9f4aeae49b3a5e6bfc9375d9b
NarcizzeN <47037769+NarcizzeN@users.noreply.github.com>
2022-06-27 19:46:34 +0100

    5:10:10 - change password working

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import {COOKIE_NAME, __prod__ } from "./constants";
+import { COOKIE_NAME, __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
@@ -8,61 +8,61 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
-import redis from "redis";
+import Redis from "ioredis";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import { MyContext } from "./types";
 
 const main = async () => {
-    const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
-    const app = express();
+  const app = express();
 
-    const RedisStore = connectRedis(session);
-    const redisClient = redis.createClient();
+  const RedisStore = connectRedis(session);
+  const redis = new Redis();
 
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        // @ts-ignore
+        client: redis,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "qowiueojwojfalksdjoqiwueo",
+      resave: false,
+    })
+  );
 
-    app.use(
-        session({
-            name: COOKIE_NAME,
-            store: new RedisStore({
-                // @ts-ignore
-                client: redisClient,
-                disableTouch: true,
-            }),
-            cookie: {
-                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
-                httpOnly: true,
-                sameSite: "lax", // csrf
-                secure: __prod__, // cookie only works in https
-            },
-            saveUninitialized: false,
-            secret: "qowiueojwojfalksdjoqiwueo",
-            resave: false,
-        })
-    );
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res, redis }),
+  });
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver, UserResolver],
-            validate: false,
-        }),
-        context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
-    });
+  apolloServer.applyMiddleware({
+    app, //this should already exist in your file
+    cors: {
+      origin: ["http://localhost:3000"],
+      credentials: true,
+    },
+  });
 
-    apolloServer.applyMiddleware({ app, //this should already exist in your file
-        cors: {
-            origin: ["http://localhost:3000",],
-            credentials: true
-        },
-    });
-
-    app.listen(4000, () => { 
-        console.log("server started on localhost:4000");
-    });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
-    console.error(err);
+  console.error(err);
 });
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -10,10 +10,11 @@ import {
 import argon2 from "argon2";
 import { MyContext } from "src/types";
 import { User } from "../entities/User";
-import { COOKIE_NAME } from "../constants";
+import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from "../constants";
 import { UsernamePasswordInput } from "./usernamePasswordInput";
 import { validateRegister } from "../util/validateRegister";
 import { sendEmail } from "../util/sendEmail";
+import { v4 } from "uuid";
 
 @ObjectType()
 class FieldError {
@@ -36,20 +37,82 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Mutation(() => UserResponse)
+  async changePassword(
+    @Arg("token") token: string,
+    @Arg("newPassword") newPassword: string,
+    @Ctx() { em, redis, req }: MyContext
+  ): Promise<UserResponse> {
+    const userId = await redis.get(FORGET_PASSWORD_PREFIX + token);
+    if (!userId) {
+      return {
+        errors: [
+          {
+            field: "token",
+            message: "token expired",
+          },
+        ],
+      };
+    }
+
+    if (newPassword.length < 2) {
+      return {
+        errors: [
+          {
+            field: "newPassword",
+            message: "password must be at least 2 characters",
+          },
+        ],
+      };
+    }
+
+    const user = await em.findOne(User, { id: parseInt(userId) });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "token",
+            message: "user no longer exists",
+          },
+        ],
+      };
+    }
+    const hashedPassword = await argon2.hash(newPassword);
+    user.password = hashedPassword;
+    await em.persistAndFlush(user);
+    // await redis.del(FORGET_PASSWORD_PREFIX + token);
+
+    //login the user:
+    req.session!.userId = user.id;
+
+
+    return { user };
+  }
+
   @Mutation(() => Boolean)
-  async fogotPassword(@Arg("email") email: string,
-                      @Ctx() { em }: MyContext
-                      ) {
-    const user = await em.findOne(User, { email})
+  async fogotPassword(
+    @Arg("email") email: string,
+    @Ctx() { em, redis }: MyContext
+  ) {
+    const user = await em.findOne(User, { email });
     if (!user) {
       // you want to return true so as to not let hackers fish the address:
       return true;
     }
 
-    const token= "40893jh543p89439"
+    const token = v4();
 
-    await sendEmail(email, `<a href="http://localhost:3000/change-password/${token}">reset password</a>`);
+    await redis.set(
+      FORGET_PASSWORD_PREFIX + token,
+      user.id,
+      "EX",
+      1000 * 60 * 60 * 24 * 3
+    ); // 3 days
 
+    await sendEmail(
+      email,
+      `<a href="http://localhost:3000/change-password/${token}">reset password</a>`
+    );
 
     return true;
   }
